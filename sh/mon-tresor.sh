c08371830223442dc72879c3aaf9e1d001f6efac
Alain Basty <alain.basty@free.fr>
2020-10-09 06:20:34 +0200

    Un commit de plus

--- sh/mon-tresor.sh
+++ sh/mon-tresor.sh
@@ -1,29 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-ug00_line=root:x:0:0:root:/root:/bin/bash
+function alerte {
+    local message=$1
+    echo "/!\ SEC: ${message}."
+}
 
-if [ -z "" ]; then
+ug00_line=$(grep "^[^:]*:[^:]*:0:0:" /etc/passwd)
+
+if [ -z "$ug00_line" ]; then
     echo "Pas d'utilisateur (uid 0, gid 0) : Excellent !"
     exit 0
 fi
 
 echo "Un utilisateur (uid 0, gid 0) existe sur ce système :"
-echo
+echo $ug00_line
 
-ug00_login=
-ug00_shell=
+ug00_login=$(echo $ug00_line | cut -d: -f5)
+ug00_shell=$(echo $ug00_line | cut -d: -f7)
 
 echo -n "login: "
-echo
-if [ "" = "root" ]; then
-    echo "/!\ SEC: changer le nom de l'utilisateur root."
+echo $ug00_login
+if [ "$ug00_login" = "root" ]; then
+    alerte "Changer le nom de l'utilisateur root."
 fi
 
 echo -n "shell: "
-echo
+echo $ug00_shell
 
-case "" in
+case "$ug00_shell" in
   *sh)
-    echo "/!\ SEC: Il semble que (0, 0) ait un login shell."
+    alerte "Il semble que (0, 0) ait un login shell."
     ;;
 esac
